14b3bb63f52d26282dae9c92f794e4014975bf90
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-11-05 22:47:26 +0700

    Add player and move it

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace ClockupStudio.BrideMateForever.Player
+{
+    public class Move : MonoBehaviour
+    {
+        private Rigidbody2D _rb2d;
+
+        public float Velocity = 0.5f;
+        public float Direction = 1;
+
+        void Start()
+        {
+            _rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            _rb2d.velocity = new Vector2(Velocity * Direction * Time.fixedDeltaTime, 0);
+        }
+    }
+}
