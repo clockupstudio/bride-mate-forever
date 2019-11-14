c84324920832dcede8ffcf1d912ca0313eede44f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-11-14 23:24:04 +0700

    Flip when move to left or right

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/AnimationDirection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClockupStudio.BrideMateForever.Player
+{
+    [RequireComponent(typeof(Direction))]
+    public class AnimationDirection : MonoBehaviour
+    {
+        private Direction _dircomp;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _dircomp = GetComponent<Direction>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_dircomp.MoveDirection == MoveDirection.NoMove) return;
+
+            var scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x) * (int)_dircomp.MoveDirection;
+            transform.localScale = scale;
+        }
+    }
+}
