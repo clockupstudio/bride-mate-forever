20005976cc27a4acb185f090782660f3349e4350
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-11-14 23:14:12 +0700

    Change set direction to a new component

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/Direction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ClockupStudio.BrideMateForever.Player
+{
+    public class Direction : MonoBehaviour
+    {
+        public MoveDirection MoveDirection = MoveDirection.Right;
+
+        public void SetDirection(MoveDirection dir)
+        {
+            Debug.Log($"Move to direction {dir}");
+            MoveDirection = dir;
+        }
+    }
+
+}
--- Assets/Scripts/ClockupStudio.BrideMateForever.Player/Move.cs
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/Move.cs
@@ -2,24 +2,21 @@
 
 namespace ClockupStudio.BrideMateForever.Player
 {
+    [RequireComponent(typeof(Direction))]
     public class Move : MonoBehaviour
     {
         public float Velocity = 0.5f;
-        public MoveDirection Direction = MoveDirection.Right;
+
+        private Direction _dircomp;
 
         private void Start()
         {
+            _dircomp = GetComponent<Direction>();
         }
 
         private void FixedUpdate()
         {
-            transform.Translate(new Vector2(Velocity * (int)Direction * Time.fixedDeltaTime, 0));
-        }
-
-        public void SetDirection(MoveDirection dir)
-        {
-            Debug.Log($"Move to direction {dir}");
-            Direction = dir;
+            transform.Translate(new Vector2(Velocity * (int)_dircomp.MoveDirection * Time.fixedDeltaTime, 0));
         }
     }
 }
