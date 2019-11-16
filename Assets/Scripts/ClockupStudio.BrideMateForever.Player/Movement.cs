ece0d52baf976b3fcdfc55af829e00ee5a5159da
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-11-16 16:28:37 +0700

    Implements jump

--- Assets/Scripts/ClockupStudio.BrideMateForever.Player/Movement.cs
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/Movement.cs
@@ -5,18 +5,27 @@ namespace ClockupStudio.BrideMateForever.Player
     [RequireComponent(typeof(InputHandler))]
     public class Movement : MonoBehaviour
     {
-        public float Velocity = 0.5f;
+        public Vector2 Velocity = new Vector2(50f, 10f);
 
         private InputHandler _handler;
+        private Rigidbody2D _rb2d;
 
         private void Start()
         {
             _handler = GetComponent<InputHandler>();
+            _rb2d = GetComponent<Rigidbody2D>();
         }
 
         private void FixedUpdate()
         {
-            transform.Translate(new Vector2(Velocity * (int)_handler.MoveDirection * Time.fixedDeltaTime, 0));
+
+            var vec2 = _rb2d.velocity;
+            vec2.x = Velocity.x * (int)_handler.MoveDirection * Time.fixedDeltaTime;
+            if (_handler.PressedJump)
+            {
+                vec2.y += _rb2d.mass + (Velocity.y * Physics2D.gravity.y * Time.fixedDeltaTime);
+            }
+            _rb2d.velocity = vec2;
         }
     }
 }
