5b2846456f48b28dec5825319980785cd213d8ee
Prayoch Rujira <prayoch@clockupstudio.com>
2019-11-24 23:20:43 +0700

    Add Dog patrolling

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Enemy/Patrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClockupStudio.BrideMateForever.Enemy
+{
+    public class Patrol : MonoBehaviour
+    {
+        public float speed;
+        public float distance;
+        private bool _isMoveRight = false;
+        public Transform groundDetection;
+        void Update()
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+            if (groundInfo.collider == false)
+            {
+                if(_isMoveRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    _isMoveRight = false;
+                }
+                else 
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    _isMoveRight = true;
+                }
+            }
+        }
+    }
+}
