4fb248328ce28404f0b519f4b27783fae7ccda86
Prayoch Rujira <prayoch@clockupstudio.com>
2019-12-01 00:44:09 +0700

    Display text when dead or win

--- Assets/Scripts/ClockupStudio.BrideMateForever.Player/PlayerDead.cs
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/PlayerDead.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace ClockupStudio.BrideMateForever.Player
 {
@@ -8,20 +9,27 @@ namespace ClockupStudio.BrideMateForever.Player
     {
         private Movement _movement;
         private Animator _animator;
+        private Rigidbody2D _body;
         public GameObject input;
+
+        public TextMeshProUGUI text;
 
         void Awake()
         {
             _movement = GetComponent<Movement>();
             _animator = GetComponent<Animator>();
+            _body = GetComponent<Rigidbody2D>();
         }
 
         public void Dead()
         {
             input.SetActive(false);
-            
             _movement.Disable(true);
+            _body.velocity = Vector2.zero;
             _animator.SetInteger("Action", 4);
+
+            text.gameObject.SetActive(true);
+            text.text = "Next time... maybe.";
         }
     }
 }
--- Assets/Scripts/ClockupStudio.BrideMateForever.Player/PlayerWin.cs
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Player/PlayerWin.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace ClockupStudio.BrideMateForever.Player
 {
@@ -9,9 +10,9 @@ namespace ClockupStudio.BrideMateForever.Player
         private Movement _movement;
         private Animator _animator;
         private Rigidbody2D _body;
-        
-        public GameObject input;
 
+        public GameObject input;
+        public TextMeshProUGUI text;
         void Awake()
         {
             _movement = GetComponent<Movement>();
@@ -25,6 +26,9 @@ namespace ClockupStudio.BrideMateForever.Player
             _movement.Disable(true);
             _body.velocity = Vector2.zero;
             _animator.SetInteger("Action", 5);
+
+            text.gameObject.SetActive(true);
+            text.text = "YEAAAHHH!!";
         }
     }
 }
