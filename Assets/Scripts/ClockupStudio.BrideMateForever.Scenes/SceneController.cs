b6110c4cfd6e800e8d4cb21dad6786ea4cc9af14
Prayoch Rujira <prayoch@clockupstudio.com>
2019-11-30 13:55:24 +0700

    Add weddding scene beginning script

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Scenes/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ClockupStudio.BrideMateForever.NPCs;
+using ClockupStudio.BrideMateForever.Player;
+
+namespace ClockupStudio.BrideMateForever.Scenes
+{
+    public class SceneController : MonoBehaviour
+    {
+        public Bride bride;
+        public Movement movement;
+        public GameObject text;
+
+        private bool _started = false;
+        
+        void Update() {
+            if(_started) {
+                return;
+            }
+
+            movement.Disable(true);
+            StartCoroutine("BeginGame");
+            Debug.Log("Start scene");
+            _started = true;
+        }
+
+
+        private IEnumerator BeginGame()
+        {
+            yield return new WaitForSeconds(1f);
+            Debug.Log("Begin scene");
+            bride.Throw();
+            text.SetActive(false);
+            movement.Disable(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ClockupStudio.BridermateForever.npcs/Bride.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClockupStudio.BrideMateForever.NPCs
+{
+    public class Bride : MonoBehaviour
+    {
+        private Animator _animator;
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Throw()
+        {
+            _animator.SetBool("Throw", true);
+        }
+
+        public void Idle()
+        {
+            _animator.SetBool("Throw", false);
+        }
+    }
+}
