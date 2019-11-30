52fd6d94c7eefa5a90439b1d4a5746512daf041a
Prayoch Rujira <prayoch@clockupstudio.com>
2019-12-01 01:41:17 +0700

    Added wedding scene portal to game scene

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Scenes/EnterWedding.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ClockupStudio.BrideMateForever.Scenes
+{
+    public class EnterWedding : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other) {
+            SceneManager.LoadScene("Wedding");
+        }
+    }
+}
