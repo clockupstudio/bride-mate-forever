ec0df17368ae1ff513d023e8c616dea820649143
Prayoch Rujira <prayoch@clockupstudio.com>
2019-12-01 16:26:37 +0700

    Add title scene script

---
+++ Assets/Scripts/ClockupStudio.BrideMateForever.Scenes/Title.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ClockupStudio.BrideMateForever.Scenes
+{
+    public class Title : MonoBehaviour
+    {
+        
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+    }
+}
+
